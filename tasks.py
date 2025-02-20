3ddf84d118e675e5ef109561947f35a7634bb9ee
Bent Mueller <datastepper@icloud.com>
2025-02-20 17:47:15 +0100

    Published 0.1.0

--- tasks.py
+++ tasks.py
@@ -7,3 +7,9 @@ def pre_commit(ctx):
     ctx.run("isort .")
     ctx.run("ruff format")
     ctx.run("ruff check --fix")
+
+@task
+def clean(ctx):
+    ctx.run("rm -rf build dist src.egg-info")
+    ctx.run("find . -name '*.pyc' -delete")
+    ctx.run("find . -name '__pycache__' -delete")
